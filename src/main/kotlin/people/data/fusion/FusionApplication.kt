b0f61b606a015f28498ce5fe94a6ea32540d28c4
YongBin Xu <xuybin@qq.com>
2018-03-05 11:30:48 +0800

    update

---
+++ src/main/kotlin/people/data/fusion/FusionApplication.kt
@@ -0,0 +1,11 @@
+package people.data.fusion
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FusionApplication
+
+fun main(args: Array<String>) {
+    runApplication<FusionApplication>(*args)
+}
